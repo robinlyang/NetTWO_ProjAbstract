815751baaa199f995f2d82dc321fd9cdc26b8a37
robinlyang <robiny@gmail.com>
2016-11-07 20:08:59 -0700

    Inital Project

--- NetTWO_ProjAbstract/Program.cs
+++ NetTWO_ProjAbstract/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace NetTWO_ProjAbstract
 {
+    abstract class sales
+    {
+        abstract public void show_sales();
+        public double taxgst(double x)
+        {
+            return x * 0.05;
+        }
+    }
+    class invoice : sales
+    {
+        public double amount = 0;
+        public override void show_sales()
+        {//you put in code
+            Console.WriteLine(amount.ToString("c"));
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            //sales x = new sales();   <- this won't work
+            invoice i = new invoice();
+            i.show_sales();
+            
         }
     }
 }
